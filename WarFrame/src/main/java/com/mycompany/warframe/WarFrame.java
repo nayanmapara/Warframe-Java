90866e041f20d801464829c84f1987a55d0c91be
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2023-07-18 15:12:08 -0400

    Added Modifier

---
+++ WarFrame/src/main/java/com/mycompany/warframe/WarFrame.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.warframe;
+
+
+public class WarFrame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

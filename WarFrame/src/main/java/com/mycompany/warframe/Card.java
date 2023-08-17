86254bec7d2e87229b087b89e7b88a90f9141ae2
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2023-08-17 18:32:51 -0400

    Added Modifier

--- WarFrame/src/main/java/com/mycompany/warframe/Card.java
+++ WarFrame/src/main/java/com/mycompany/warframe/Card.java
@@ -1,25 +1,37 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
 package com.mycompany.warframe;
 
+// Card.java
+
+
 /**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
+ * A class to represent a playing card.
+ * Each card has a suit and a value.
+ * The suit can be Hearts, Diamonds, Clubs, or Spades.
+ * The value can range from 2 to 14 (11 = Jack, 12 = Queen, 13 = King, 14 = Ace).
+ * The card's value determines its rank in the game.
+ * For this implementation, we assume that higher values represent higher ranks.
  * @author dancye
  */
-public abstract class Card {
-    //default modifier for child classes
+public class Card {
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    private final String suit;
+    private final int value;
+
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
     @Override
-    public abstract String toString();
-
+    public String toString() {
+        return value + " of " + suit;
+    }
 }

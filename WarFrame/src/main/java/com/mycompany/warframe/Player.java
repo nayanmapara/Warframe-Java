a0aee1ab0135956bb9f8e589610b73a2c14c0a66
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2023-08-17 18:32:36 -0400

    Added Modifier

--- WarFrame/src/main/java/com/mycompany/warframe/Player.java
+++ WarFrame/src/main/java/com/mycompany/warframe/Player.java
@@ -1,50 +1,48 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
 package com.mycompany.warframe;
 
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
+import java.util.ArrayList;
 
-public abstract class Player {
+public class Player {
 
-    private String name; //the unique name for this player
+    private String name;
+    private Deck hand;
+    private ArrayList<Round> roundsPlayed;
 
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
     public Player(String name) {
         this.name = name;
+        this.hand = new Deck();
+        this.roundsPlayed = new ArrayList<>();
     }
 
-    /**
-     * @return the player name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
-    public void setName(String name) {
-        this.name = name;
+    public Deck getHand() {
+        return hand;
     }
 
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
+    public Card drawCard() {
+        return hand.drawCard();
+    }
 
+    public void addToHand(Card card) {
+        hand.addCard(card);
+    }
+
+    public void removeFromHand(int index) {
+        hand.removeCard(index);
+    }
+
+    public int getHandSize() {
+        return hand.getSize();
+    }
+
+    public void addRoundPlayed(Round round) {
+        roundsPlayed.add(round);
+    }
+
+    public ArrayList<Round> getRoundsPlayed() {
+        return roundsPlayed;
+    }
 }
